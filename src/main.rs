f14bbb9e8a79724d030372d2b9114f163be6e1b3
Marcos Roque <devmarcosdanielr@gmail.com>
2025-06-01 04:01:02 -0300

    chore: add env to port and server log

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod infra;
 
 mod state;
 
+use std::env;
+
 use application::http::routes::{auth::auth_routes, users::users_routes};
+use dotenvy::dotenv;
 use infra::database::connection::establish_db_connection;
 
 use axum::Router;
@@ -12,14 +15,21 @@ use state::AppState;
 
 #[tokio::main]
 async fn main() {
-    let db = establish_db_connection().await;
+    dotenv().ok();
 
+    let port = env::var("PORT").unwrap_or_else(|_| "4000".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+
+    let db = establish_db_connection().await;
     let app_state = AppState { db: db.into() };
 
     let app = Router::new()
         .nest("/api/auth", auth_routes().with_state(app_state.clone()))
         .nest("/api/users", users_routes().with_state(app_state.clone()));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+
+    println!("Server running on http://{}", addr);
+
     axum::serve(listener, app).await.unwrap();
 }
